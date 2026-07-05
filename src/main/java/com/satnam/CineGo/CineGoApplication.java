0b4c049fb41825664779b3dcffba235838b9c39d
Satnam Singh <singhsatnam050804@gmail.com>
2026-07-06 00:48:14 +0530

    Project setup

---
+++ src/main/java/com/satnam/CineGo/CineGoApplication.java
@@ -0,0 +1,13 @@
+package com.satnam.CineGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/satnam/CineGo/CineGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.satnam.CineGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
